386fcaba27453eb7632a9c0905d132a0630c18a6
CoDanTheBarbarian <plafcandaniel@gmail.com>
2024-02-07 13:29:59 -0800

    Added the draw_move() method to the Cell class.

--- maze_solver.py
+++ maze_solver.py
@@ -55,7 +55,7 @@ class Cell:
         self.has_right_wall = right
         self.win = canvas
 
-def draw_cell(self):
+    def draw_cell(self):
         top_left_point = Point(self.x_1, self.y_1)
         top_right_point = Point(self.x_2, self.y_1)
         bottom_right_point = Point(self.x_2, self.y_2)
@@ -71,8 +71,17 @@ def draw_cell(self):
             bottom.draw(self.win, "red")
         if self.has_right_wall:
             right = Line(top_right_point, bottom_right_point)
-            right.draw(self.win, "red")   
+            right.draw(self.win, "red")
 
+    def draw_move(self, to_cell, undo=False):
+        point_1 = Point((self.x_1 + self.x_2) / 2, (self.y_1 + self.y_2) / 2)
+        point_2 = Point((to_cell.x_1 + to_cell.x_2) / 2, (to_cell.y_1 + to_cell.y_2) / 2)
+        foreward_path = Line(point_1, point_2)
+        reverse_path = Line(point_2, point_1)
+        if not undo:
+            foreward_path.draw(self.win, "red")
+        else:
+            reverse_path.draw(self.win, "gray")
 
 def main():
     win = Window(800, 600)
