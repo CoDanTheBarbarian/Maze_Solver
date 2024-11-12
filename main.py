c82855b49728b0c8cecdeb12a85fdaa3ed0ced75
CoDanTheBarbarian <plafcandaniel@gmail.com>
2024-11-12 13:36:54 -0800

    Got the recursive search algorithm working.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from maze import Maze
 from graphics import Window
-
+import sys
 
 def main():
     num_columns = 16
@@ -10,11 +10,17 @@ def main():
     screen_y = 600
     cell_x_size = (screen_x - 2 * margin) / num_columns
     cell_y_size = (screen_y - 2 * margin) / num_rows
-    
+
+    sys.setrecursionlimit(10000)
     win = Window(screen_x, screen_y)
     
     maze = Maze(margin, margin, num_rows, num_columns, cell_x_size, cell_y_size, win, 10)
-
+    print("Maze created")
+    is_solvable = maze.solve()
+    if not is_solvable:
+        print("Maze is unsolvable")
+    else:
+        print("Maze is solvable")
     win.wait_for_close()
 
 if __name__ == "__main__":
